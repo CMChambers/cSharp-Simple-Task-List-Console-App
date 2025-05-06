9983a97e057ea840da7a2be8b9a95f8c821c6569
Chad M Chambers <61452826+CMChambers@users.noreply.github.com>
2025-05-06 14:17:21 -0400

    update/ move app class out of main

--- Program.cs
+++ Program.cs
@@ -4,56 +4,7 @@ public class Program
 {
     static void Main()
     {
-        var taskList = new TaskList();
-        bool running = true;
-
-        while (running)
-        {
-            WriteMenu();
-
-            var menuSelection = Console.ReadLine();
-
-            switch (menuSelection)
-            {
-                case "1":
-                    Console.Write("Enter task description: ");
-                    string description = Console.ReadLine();
-                    taskList.AddTask(description);
-                    break;
-                case "2":
-                    taskList.ViewTask();
-                    Console.ReadKey();
-                    break;
-                case "3":
-                    Console.Write("Enter task ID to complete: ");
-                    if (int.TryParse(Console.ReadLine(), out int completeId))
-                    {
-                        taskList.CompleteTask(completeId);
-                    }
-                    break;
-                case "4":
-                    Console.Write("Enter task ID to delete: ");
-                    if (int.TryParse(Console.ReadLine(), out int deleteId))
-                    {
-                        taskList.DeleteTask(deleteId);
-                    }
-                    break;
-                case "5":
-                    running = false;
-                    break;
-            }
-        }
-    }
-
-    public static void WriteMenu()
-    {
-        Console.Clear();
-        Console.WriteLine("Simple To-Do List");
-        Console.WriteLine("1. Add Task");
-        Console.WriteLine("2. View Task");
-        Console.WriteLine("3. Complete Task");
-        Console.WriteLine("4. Delete Task");
-        Console.WriteLine("5. Exit");
-        Console.Write("Enter your choice: ");
+        var taskApp = new TaskApp();
+        taskApp.Run();
     }
 }
